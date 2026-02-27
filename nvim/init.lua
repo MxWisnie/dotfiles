5ab275ec666ce131338f6e25cae4c09d6b60c88d
MaksWisniewski <makswisnie@gmail.com>
2026-02-27 16:53:42 +0100

    new config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
-require("config.lazy")
+-- ~/.config/nvim/init.lua
 
+require("core.options")
+require("core.keymaps")
+require("core.autocmds")
+require("core.statusline")
+require("core.terminal")
+
+require("plugins.init")
+require("plugins.lsp")
